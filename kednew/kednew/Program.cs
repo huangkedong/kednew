1587f1a14ae154b992b6c455236e47a5906b69cd
huangkedong <ked.huang@quantacn.com>
2015-11-30 15:29:10 +0800

    watchspace<250sendmail

--- kednew/kednew/Program.cs
+++ kednew/kednew/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using kednew.MailsSender;
 
 namespace kednew
 {
@@ -10,8 +12,17 @@ namespace kednew
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("this is first line");
-            Console.WriteLine("this is second line");
+            MailApp SMails = new MailApp();
+            string _ComputName = System.Net.Dns.GetHostName();
+            DriveInfo driveinfo = new DriveInfo(args[0].ToString());
+            long a=driveinfo.AvailableFreeSpace/1024/1024/1024;
+            int b = (int)a;
+            StringBuilder _body = new StringBuilder();
+            _body.AppendFormat("{0}上的{1}盘还有备份空间{2}G", _ComputName, args[0].ToString(),b);
+            if (b <= 250)
+            {
+                SMails.SendMailbyID("Basis", "备份磁盘监控", _body.ToString());
+            }
         }
     }
 }
